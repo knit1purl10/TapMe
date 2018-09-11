0737371e0f550e5848af3c8f60139aa15de0edc6
Olivia Hon <olivia.hon@gmail.com>
2018-09-11 15:29:21 -0400

    TapMe v1
    First iOS application. Initial commit of working version.

--- TapMe/ViewController.swift
+++ TapMe/ViewController.swift
@@ -20,6 +20,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var player1ScoreLabel: UILabel!
+    @IBOutlet weak var player2ScoreLabel: UILabel!
+    
+    var player1Score = 0
+    var player2Score = 0
+    var isGameStarted = false
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        isGameStarted = true
+        player1Score = 0
+        player2Score = 0
+        updateUI()
+        
+        Timer.scheduledTimer(withTimeInterval: 10, repeats: false) { (_) in
+            sender.isEnabled = true
+            self.isGameStarted = false
+        }
+        
+    }
+    
+    @IBAction func player1ButtonPressed(_ sender: Any) {
+        player1Score += 1
+        updateUI()
+    }
+    
+    @IBAction func player2ButtonPressed(_ sender: Any) {
+        player2Score += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        if isGameStarted {
+            player1ScoreLabel.text = "Player 1 Score: \(player1Score)"
+            player2ScoreLabel.text = "Player 2 Score: \(player2Score)"
+        }
+    }
+    
 }
 
